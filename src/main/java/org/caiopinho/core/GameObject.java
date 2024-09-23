61d1c347c1f69fcd6edf0d804ce2ca83289db185
caiopinho <caiopinho9@gmail.com>
2024-09-23 20:48:52 -0300

    fix: transform as component

--- src/main/java/org/caiopinho/core/GameObject.java
+++ src/main/java/org/caiopinho/core/GameObject.java
@@ -27,14 +27,12 @@ public class GameObject {
 	private boolean selectable = true;
 
 	public GameObject(String name) {
-		this.init(name, new Transform());
+		this.name = name;
+		this.components = new ArrayList<>();
+		this.uid = ID_COUNTER++;
 	}
 
 	public GameObject(String name, Transform transform) {
-		this.init(name, transform);
-	}
-
-	private void init(String name, Transform transform) {
 		this.name = name;
 		this.components = new ArrayList<>();
 		this.transform = transform;
